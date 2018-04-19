7960502e1212498d7a96e8f65b33b08208b0c971
Thomas Weng <thomas.weng11@gmail.com>
2018-04-19 19:09:48 -0400

    Add all termination conditions, print statements

--- fetch.py
+++ fetch.py
@@ -2,6 +2,7 @@
 Fetch all metadata from a single publication / conference year.
 """
 
+import time
 import json
 import requests
 
@@ -21,21 +22,47 @@ data = load_existing_data(outfile_name)
 
 # Request parameters
 url = "http://ieeexploreapi.ieee.org/api/v1/search/articles"
+max_record = 25
 params = {
     "publication_title": cfg['publication_title'],
     "apikey": cfg['apikey'],
-    "max_record": "200"
+    "max_record": str(max_record)
 }
 
 # Request constraints
-num_calls = 0,
+num_calls = 0
 max_calls = 175
-sleep_duration = 0.15
+start_record = 1 # TODO parametrize
+total_records = None
+sleep_duration = 0.5
 
-# Make requests
-while num_calls < max_calls:
+# Call API in loop
+def should_terminate(num_calls, max_calls, start_record, total_records):
+    return num_calls > max_calls or \
+        (total_records is not None and start_record > total_records)
+
+while not should_terminate(num_calls, max_calls, start_record, total_records):
+    # Make request
+    params["start_record"] = start_record
+    print("Requesting...")
+    print(params)
     r = requests.get(url, params=params)
-    data = r.json() # TODO append
+    req_json = r.json()
+
+    # Append to existing data
+    print("Appending data...")
+    if data == {}:
+        data = req_json
+    else:
+        data["articles"].append(req_json["articles"])
+
+    # Update loop variables
+    num_calls += 1
+    start_record += max_record
+    total_records = req_json["total_records"]
+    print("num_calls: " + str(num_calls))
+    print("start_record: " + str(start_record))
+    time.sleep(sleep_duration)
 
 # Save data
 with open(outfile_name, 'w') as outfile:
