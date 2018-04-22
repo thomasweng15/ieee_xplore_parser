f18cef605d9baa70fb88cf79267cfbac131a6a98
Thomas Weng <thomas.weng11@gmail.com>
2018-04-22 10:32:23 -0400

    Remove num and max call vars

--- fetch.py
+++ fetch.py
@@ -17,6 +17,18 @@ def load_existing_data(fname):
     except FileNotFoundError:
         return {}
 
+def get_start_record(data):
+    return 1 if data == {} else data["articles"][-1]["rank"] + 1
+
+def should_terminate(start_record, total_records):
+    return total_records is not None and start_record > total_records
+
+def update_start_record(data, start_record, max_record, total_records):
+    num_downloaded = data["articles"][-1]["rank"]
+    return num_downloaded + 1 \
+        if start_record + max_record > total_records and num_downloaded < int(total_records) \
+        else start_record + max_record
+
 outfile_name = 'data/' + cfg['outfile'].replace("{0}", cfg['year'])
 data = load_existing_data(outfile_name)
 
@@ -26,33 +38,19 @@ max_record = 200
 params = {
     "publication_title": '"' + cfg['publication_title'] + '"',
     "apikey": cfg['apikey'],
-    "max_record": str(max_record),
+    "max_record": str)max_record(,
     "start_year": cfg['year'],
     "end_year": cfg['year']
 }
 
-# Request constraints
-def get_start_record(data):
-    return 1 if data == {} else data["articles"][-1]["rank"] + 1
 
-num_calls = 0
-max_calls = 175
+# Request constraints
 start_record = get_start_record(data)
 total_records = None
 sleep_duration = 0.5
 
 # Call API in loop
-def should_terminate(num_calls, max_calls, start_record, total_records):
-    return num_calls > max_calls or \
-        (total_records is not None and start_record > total_records)
-
-def update_start_record(data, start_record, max_record, total_records):
-    num_downloaded = data["articles"][-1]["rank"]
-    return num_downloaded + 1 \
-        if start_record + max_record > total_records and num_downloaded < int(total_records) \
-        else start_record + max_record
-
-while not should_terminate(num_calls, max_calls, start_record, total_records):
+while not should_terminate(start_record, total_records):
     # Make request
     params["start_record"] = start_record
     print("Requesting...")
@@ -71,7 +69,6 @@ while not should_terminate(num_calls, max_calls, start_record, total_records):
     num_calls += 1
     total_records = req_json["total_records"]
     start_record = update_start_record(data, start_record, max_record, total_records)
-    print("num_calls: " + str(num_calls))
     print("start_record: " + str(start_record))
     time.sleep(sleep_duration)
 
