957b2ed948deae6cce67d50699142bb2de726282
Thomas Weng <thomas.weng11@gmail.com>
2018-04-25 13:44:30 -0400

    Finished json to csv

--- json_to_csv.py
+++ json_to_csv.py
@@ -9,10 +9,20 @@ import csv
 dirs = ["HRI", "ICRA", "IROS", "RO-MAN"]
 path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data")
 
+data = []
 for d in dirs:
     dir_path = os.path.join(path, d)
     fns = os.listdir(dir_path)
     for fn in fns: 
-        data = json.load(open(dir_path + "/" + fn))
+        j = json.load(open(dir_path + "/" + fn))
         
-        print(data)
+        for a in j["articles"]:
+            if "title" not in a.keys() or "abstract" not in a.keys():
+                continue
+
+            data.append([a["title"], a["abstract"]])
+
+with open('data.csv', 'w') as csvfile:
+    writer = csv.writer(csvfile, delimiter=',')
+    for d in data:
+        writer.writerow(d)
